820ebd6622e050bd905e74a173ff1c9c0f2fd2ee
Ullas M S <33162102+uniqueullas@users.noreply.github.com>
2020-08-11 13:24:52 +0530

    1.Added with Manual mode buttons 2.Icon added for the window 3.Changed the arrangement of buttons

--- User_input_GUI.py
+++ User_input_GUI.py
@@ -9,16 +9,19 @@ def User_input_GUI():
     root = Tk()
     root.geometry("601x700")
     root.title("Telaverge Communications")
+    root.iconbitmap("TELALOGO.ico")
     title = tkFont.Font(family="impact", size = 30)
     heading_1 = tkFont.Font(family="Lucida Grande", size=10, )
     heading_2 = tkFont.Font(family="Lucida Grande", size=10)
 
-    myLabel1 = Label(root, text="Density Based", fg = 'black', font=title)
+    myLabel1 = Label(root, text="Density", fg = 'black', font=title)
     myLabel1.grid(row=0, column=0)
-    myLabel1f = Label(root, text="Smart Traffic", fg='black', font=title)
+    myLabel1f = Label(root, text=" Based Smart", fg='black', font=title)
     myLabel1f.grid(row=0, column=1)
-    myLabel1l = Label(root, text="Control", fg='black', font=title)
+    myLabel1l = Label(root, text=" Traffic", fg='black', font=title)
     myLabel1l.grid(row=0, column=2)
+    myLabel1l1 = Label(root, text=" Control", fg='black', font=title)
+    myLabel1l1.grid(row=0, column=3)
 
     def login():
         gap_l = Label(root, text="", fg='black', font=heading_1)
@@ -63,8 +66,11 @@ def User_input_GUI():
                     gui(CURRENT_TRAFFIC_MODE)
 
         loggin = Button(root, text="login", command=check, padx=30, pady=4, fg='blue', font=heading_2)
+        #photo = PhotoImage(file="TELALOGO.png")
+        #loggin = Button(root, text="login", image=photo, command=check, padx=30, pady=4, fg='blue', font=heading_2)
         loggin.grid(row=8, column=1)
         root.mainloop()
+
 
     def gui(CURRENT_TRAFFIC_MODE):
         CURRENT_TRAFFIC_MODE = CURRENT_TRAFFIC_MODE
@@ -81,6 +87,55 @@ def User_input_GUI():
         VEHICLE_PASS_TIME=Entry(root, font=heading_1)
         VEHICLE_PASS_TIME.grid(row=3, column=1)
 
+
+        def run_this():
+            print("this is running")
+
+            myLabel6 = Label(root, text="CURRENT_MODE:", fg='black', font=heading_1)
+            myLabel6.grid(row=6, column=0)
+            myla = Label(root, text=CURRENT_TRAFFIC_MODE, fg='black', font=heading_1)
+            myla.grid(row=6, column=1)
+            gap_9 = Label(root, text="", fg='black', font=heading_1)
+            gap_9.grid(row=9, column=2)
+
+            myLabei1 = Label(root, text="Side A", fg='black', font=heading_2)
+            myLabei1.grid(row=10, column=1)
+            my_img1 = Image.open("Shapes.png")
+            resized1 = my_img1.resize((100, 100), Image.ANTIALIAS)
+            new_pic1 = ImageTk.PhotoImage(resized1)
+            SideAL = Label(image=new_pic1)
+            SideAL.grid(row=11, column=1)
+
+            myLabei2 = Label(root, text="Side B", fg='black', font=heading_2)
+            myLabei2.grid(row=12, column=0)
+            my_img2 = Image.open("Shapes.png")
+            resized2 = my_img2.resize((100, 100), Image.ANTIALIAS)
+            new_pic2 = ImageTk.PhotoImage(resized2)
+            SideBL = Label(image=new_pic2)
+            SideBL.grid(row=13, column=0)
+
+            gap_12 = Label(root, text="", fg='black', font=heading_1)
+            gap_12.grid(row=13, column=1)
+
+            myLabei3 = Label(root, text="Side C", fg='black', font=heading_2)
+            myLabei3.grid(row=12, column=2)
+            my_img3 = Image.open("Shapes.png")
+            resized3 = my_img3.resize((100, 100), Image.ANTIALIAS)
+            new_pic3 = ImageTk.PhotoImage(resized3)
+            SideCL = Label(image=new_pic3)
+            SideCL.grid(row=13, column=2)
+
+            myLabei4 = Label(root, text="Side D", fg='black', font=heading_2)
+            myLabei4.grid(row=14, column=1)
+            my_img4 = Image.open("Shapes.png")
+            resized4 = my_img4.resize((100, 100), Image.ANTIALIAS)
+            new_pic4 = ImageTk.PhotoImage(resized4)
+            SideDL = Label(image=new_pic4)
+            SideDL.grid(row=15, column=1)
+            button_quit = Button(root, text="exit", command=root.quit)
+            button_quit.grid(row=20, column=1)
+            root.mainloop()
+
         def submit():
             print("MINIMUM_VEHICLE :",MINIMUM_VEHICLE.get())
             print("TRAFFIC_TIME :",TRAFFIC_TIME.get())
@@ -89,71 +144,67 @@ def User_input_GUI():
 
         mybut = Button(root,text = "Submit all variables!", command = submit, padx =20, pady =5, fg='blue',font= heading_2)
         mybut.grid(row=4, column=1)
+        gap_5 = Label(root, text="", fg='black', font=heading_1)
+        gap_5.grid(row=5, column=2)
 
         def auto():
             CURRENT_TRAFFIC_MODE = "Auto Mode  "
-            print("CURRENT_TRAFFIC_MODE:",CURRENT_TRAFFIC_MODE)
-            c = ("CURRENT_TRAFFIC_MODE: " + CURRENT_TRAFFIC_MODE)
-            myLabel6 = Label(root, text=c, fg='black', font=heading_1)
+            print("CURRENT_MODE:",CURRENT_TRAFFIC_MODE)
+            myLabel6 = Label(root, text="CURRENT_MODE:" , fg='black', font=heading_1)
             myLabel6.grid(row=6, column=0)
+            myla =  Label(root, text= CURRENT_TRAFFIC_MODE, fg='black', font=heading_1)
+            myla.grid(row=6, column=1)
+            run_this()
 
         def manual():
             CURRENT_TRAFFIC_MODE = "Manual Mode"
-            print("CURRENT_TRAFFIC_MODE:", CURRENT_TRAFFIC_MODE)
-            c = ("CURRENT_TRAFFIC_MODE: " + CURRENT_TRAFFIC_MODE)
-            myLabel6 = Label(root, text=c, fg='black', font=heading_1)
+            print("CURRENT_MODE:", CURRENT_TRAFFIC_MODE)
+            myLabel6 = Label(root, text="CURRENT_MODE:", fg='black', font=heading_1)
             myLabel6.grid(row=6, column=0)
+            myla = Label(root, text=CURRENT_TRAFFIC_MODE, fg='black', font=heading_1)
+            myla.grid(row=6, column=1)
 
-        myLabel5 = Label(root, text="", fg='black', font=heading_1)
-        myLabel5.grid(row=5, column=0)
-        c = ("CURRENT_TRAFFIC_MODE: " + CURRENT_TRAFFIC_MODE)
-        myLabel6 = Label(root, text=c, fg='black', font=heading_1)
-        myLabel6.grid(row=6, column=0)
+            photoa = Image.open("arrow_up.png")
+            resizeda = photoa.resize((96, 96), Image.ANTIALIAS)
+            newphotoa = ImageTk.PhotoImage(resizeda)
 
+            photob = Image.open("arrow_left.png")
+            resizedb = photob.resize((96, 96), Image.ANTIALIAS)
+            newphotob = ImageTk.PhotoImage(resizedb)
+
+            photoc = Image.open("arrow_right.png")
+            resizedc = photoc.resize((96, 96), Image.ANTIALIAS)
+            newphotoc = ImageTk.PhotoImage(resizedc)
+
+            photod = Image.open("arrow_down.png")
+            resizedd = photod.resize((96, 96), Image.ANTIALIAS)
+            newphotod = ImageTk.PhotoImage(resizedd)
+
+            arrowa = Button(root, text="up", image=newphotoa, padx=60, pady=60, fg='blue', font=heading_2)
+            arrowa.grid(row=11, column=1)
+            arrowb = Button(root, text="left", image=newphotob, padx=60, pady=60, fg='blue', font=heading_2)
+            arrowb.grid(row=13, column=0)
+            arrowc = Button(root, text="right", image=newphotoc, padx=60, pady=60, fg='blue', font=heading_2)
+            arrowc.grid(row=13, column=2)
+            arrowd = Button(root, text="down", image=newphotod, padx=60, pady=60, fg='blue', font=heading_2)
+            arrowd.grid(row=15, column=1)
+            root.mainloop()
+
+            # gap_9
+            # myLabei1
+            #SideAL.grid_forget()
+            # myLabei2
+            # SideBL
+            # myLabei3
+            # SideCL
+            # myLabei4
+            # SideDL
+
+        gap_7 = Label(root, text="", fg='black', font=heading_1)
+        gap_7.grid(row=7, column=2)
         Modesel_smart = Button(root, text="Auto Mode", command=auto, padx=20, pady=5, fg='red', font=heading_2)
-        Modesel_smart.grid(row=7, column=0)
+        Modesel_smart.grid(row=8, column=0)
         Modesel_normal = Button(root, text="Manual Mode", command=manual, padx=20, pady=5, fg='red', font=heading_2)
-        Modesel_normal.grid(row=7, column=1)
-
-        gap_9 = Label(root, text="", fg='black', font=heading_1)
-        gap_9.grid(row=9, column=2)
-
-        myLabei1 = Label(root, text="Side A", fg='black', font=heading_2)
-        myLabei1.grid(row=10, column=0)
-        my_img1 = Image.open("Shapes.png")
-        resized1 = my_img1.resize((100, 100), Image.ANTIALIAS)
-        new_pic1 = ImageTk.PhotoImage(resized1)
-        SideAL = Label(image=new_pic1)
-        SideAL.grid(row=11, column=0)
-
-        myLabei2 = Label(root, text="Side B", fg='black', font=heading_2)
-        myLabei2.grid(row=10, column=1)
-        my_img2 = Image.open("Shapes.png")
-        resized2 = my_img2.resize((100, 100), Image.ANTIALIAS)
-        new_pic2 = ImageTk.PhotoImage(resized2)
-        SideAL = Label(image=new_pic2)
-        SideAL.grid(row=11, column=1)
-
-        gap_12 = Label(root, text="", fg='black', font=heading_1)
-        gap_12.grid(row=12, column=2)
-
-        myLabei3 = Label(root, text="Side C", fg='black', font=heading_2)
-        myLabei3.grid(row=13, column=0)
-        my_img3 = Image.open("Shapes.png")
-        resized3 = my_img3.resize((100, 100), Image.ANTIALIAS)
-        new_pic3 = ImageTk.PhotoImage(resized3)
-        SideAL = Label(image=new_pic3)
-        SideAL.grid(row=14, column=0)
-
-        myLabei4 = Label(root, text="Side D", fg='black', font=heading_2)
-        myLabei4.grid(row=13, column=1)
-        my_img4 = Image.open("Shapes.png")
-        resized4 = my_img4.resize((100, 100), Image.ANTIALIAS)
-        new_pic4 = ImageTk.PhotoImage(resized4)
-        SideAL = Label(image=new_pic4)
-        SideAL.grid(row=14, column=1)
-
-        button_quit = Button(root, text="exit", command=root.quit)
-        button_quit.grid(row=20, column=1)
-        root.mainloop()
+        Modesel_normal.grid(row=8, column=1)
+        run_this()
     login()
--- try.py
+++ try.py
@@ -8,7 +8,7 @@ height=int(img.shape[0]*scale_percentage)
 dimesion=(width,height)
 
 resized=cv2.resize(img,dimesion,interpolation=)
-"""
+
 
 from tkinter import *
 from PIL import ImageTk, Image
@@ -26,9 +26,30 @@ mylabel.pack(pady=20)
 button_quit = Button(root,text="exit",command=root.quit)
 button_quit.pack()
 root.mainloop()
+"""
+
+def hu():
+    print("i am hu")
+
+def numbers_to_strings(argument):
+    switchr = {
+        'Manual Mode': hu(),
+        1: "one",
+        2: "two",
+    }
+
+    # get() method of dictionary data type returns
+    # value of passed argument if it is present
+    # in dictionary otherwise second argument will
+    # be assigned as default value of passed argument
+    #return switcher.get(argument, "nothing")
+    return switchr.get(argument,'hu()')
 
 
-
+# Driver program
+#if __name__ == "__main__":
+argument = 'Manual Mode'
+print (numbers_to_strings(argument))
 
 
 
