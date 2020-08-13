3abe60794873d7780b9b1f738cbf13717a2f6ec3
sachin b s <69026179+sachinbs1995@users.noreply.github.com>
2020-08-13 16:50:27 +0530

    multiple threading issue solved
    Tested with smart mode input to normal mode input an vs

--- smartMode.py
+++ smartMode.py
@@ -24,22 +24,24 @@ def led_control(signal_time, road_led):
         current_time = (int(round(time.time())))
         if current_time == print_time:
             print_time = (int(round(time.time()+1)))
+            print("")
             print("Elapsing time:", total_signal_time - present_time)
             present_time = (int(round(time.time())))
     print("--------------------led control over")
 
 
 def normal_mode(road_normal):
-    print("-----normal mode-----", end='')
+    print("--------------------normal mode--------------", end='')
     nt1 = threading.Thread(target=led_control, args=(5, road_normal,))
     nt2 = threading.Thread(target=img_capture, args=())
     nt1.start()
     nt2.start()
+    nt1.join()
     nt2.join()
 
 
 def smart_mode(time_per_vehicle_smart, vehicle_count_smart, road_smart):
-    print("-----smart_mode-----", end='')
+    print("---------------------smart_mode----------------", end='')
     signal_time = (vehicle_count_smart * time_per_vehicle_smart)
     st1 = threading.Thread(target=led_control, args=(signal_time, road_smart,))
     st2 = threading.Thread(target=img_capture, args=())
@@ -54,4 +56,5 @@ def img_capture():
     while image_capture_variable:
         pass
     it = (input("-----vehicle_count:"))
+    print("", end='')
     vehicle_count.append(int(it))
